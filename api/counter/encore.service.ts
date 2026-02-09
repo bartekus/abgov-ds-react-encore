c4fca75a5cbe0d2f8c9bb8f0dfa691c2262f1a95
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2026-02-09 09:57:19 -0700

    Baseline

--- api/counter/encore.service.ts
+++ api/counter/encore.service.ts
@@ -2,4 +2,4 @@ import { Service } from "encore.dev/service";
 
 // Encore will consider this directory and all its subdirectories as part of the "admin" service.
 // https://encore.dev/docs/ts/primitives/services
-export default new Service("api");
+export default new Service("counter");
