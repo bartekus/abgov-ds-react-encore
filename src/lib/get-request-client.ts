9005d32b50263c08467d84ef42335144372f9456
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2026-02-11 19:20:36 -0700

    Baseline

--- src/lib/get-request-client.ts
+++ src/lib/get-request-client.ts
@@ -1,5 +1,7 @@
 import Client, { BaseURL, Local } from "./client";
 
-const baseURL: BaseURL = (import.meta.env.VITE_API_BASE_URL as string | undefined) || Local;
+console.log("import.meta.env", import.meta.env);
+
+const baseURL: BaseURL = import.meta.env.DEV ? Local : (import.meta.env.VITE_API_BASE_URL as string);
 
 export const api = new Client(baseURL);
