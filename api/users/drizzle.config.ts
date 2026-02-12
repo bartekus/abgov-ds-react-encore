ef98021e27e33d2ab668515e9a3de900fc899d4f
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2026-02-12 10:17:23 -0700

    Baseline

--- api/users/drizzle.config.ts
+++ api/users/drizzle.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-  out: 'migrations',
-  schema: 'schema.ts',
-  dialect: 'postgresql',
+  out: "./migrations",
+  schema: "./schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+    ssl: {
+      ca: process.env.DATABASE_CA,
+    },
+  },
 });
